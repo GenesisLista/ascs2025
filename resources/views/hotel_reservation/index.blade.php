6bcdf1182129ac8c5c99c9c7dddd7d4566a98ca6
Genesis <genesis.bergonia.lista@gmail.com>
2025-05-02 11:24:40 +0800

    Update hotel page

--- resources/views/hotel_reservation/index.blade.php
+++ resources/views/hotel_reservation/index.blade.php
@@ -8,40 +8,60 @@
             <h2 class="v3">Hotel Reservations</h2>
 
             <div class="row">
-                <div class="grid_12">
-                    <p class="txt12_welcome_page">
-                        Page is under construction. Please check back later.
-                    </p>
+                <div class="grid_6">
+                    <img src="{{ asset('images/hotel/grand-westside-hotel.jpg') }}"
+                        alt="PSCS - ASCS - Gran Westside Hotel Philippines" class="img7_pscs_events">
                 </div>
-            </div>
 
-            <div class="row">
                 <div class="grid_6">
                     <p class="txt12_welcome_page">
-                        Okada Hotel
+                        Grand Westside
                     </p>
                     <p>
-                        Okada Manila is a luxury hotel and casino resort located in Entertainment City, Para&#241;aque,
-                        Metro
-                        Manila, Philippines. It is one of the largest integrated resorts in the country and features a
-                        wide range of amenities, including a casino, restaurants, bars, a spa, and a large swimming
-                        pool. The hotel is known for its opulent design and luxurious accommodations.
+                        Experience the best of leisure and entertainment at Grand Westside Hotel, the largest hotel in
+                        the Philippines, located in Westside City, Entertainment City, Parañaque, near the stunning
+                        Manila
+                        Bay. With 1,500 modern rooms and suites, this vibrant hotel is perfect for families,
+                        vacationers, and
+                        corporate groups looking for a relaxing and exciting stay.
+                    </p>
+
+                    <p>
+                        Guests can enjoy signature Filipino cuisine at LaMeza, soothing spa treatments, a fully equipped
+                        fitness center, and an outdoor pool with scenic bay views. The hotel also offers Muslim-friendly
+                        dining
+                        options, ensuring an inclusive experience for all travelers.
+                    </p>
+
+                    <p>
+                        Just 15 minutes from Ninoy Aquino International Airport (NAIA), Grand Westside Hotel provides
+                        easy access to shopping malls, entertainment venues, convention centers, and cultural
+                        attractions,
+                        making it the perfect staycation, getaway, or corporate retreat in Manila’s premier leisure
+                        district.
+                    </p>
+
+                    <p>
+                        Grand Westside is a <span style="font-weight: bold;">Halal Certified</span> hotel, ensuring that
+                        all food and beverage offerings meet Islamic dietary requirements.
                     </p>
 
                     <ul class="listWithMarker">
-                        <li><a href="https://okadamanila.com/" target="_blank">Offical Website</a></li>
-                        <li><a href="javascript:void(0)">Buffet Options</a></li>
+                        <li><a href="{{ asset('images/hotel/Grand-Westside-Hotel-Rates.pdf') }}" target="_blank">Check
+                                the rates</a></li>
                     </ul>
 
+                    <p>
+                        Click the <span style="font-weight: bold;">BOOK NOW Button</span> and use the promo code below
+                        to get a corporate rate: <span style="font-weight: bold;">GWHASCS2025</span>
+                    </p>
+
                     <div class="form_btns">
-                        <a href="https://www.booking.com/searchresults.en-gb.html?aid=2276374&label=msn-zSIekVxvaEqXMkyBSPNOUg-80127048796968%3Atikwd-80127196651061%3Aloc-149%3Aneo%3Amte%3Alp145641%3Adec%3Aqsokada%20hotel&utm_medium=cpc&utm_campaign=Hotel%20-%20Japan&utm_term=zSIekVxvaEqXMkyBSPNOUg&msclkid=ceb929dc1c5916734e2741df72ba45c4&utm_source=bing&highlighted_hotels=369159&redirected=1&city=-228233&hlrd=no_dates&source=hotel&expand_sb=1&keep_landing=1&sid=d1981482be9c4d86d221c1f9e336c10a"
-                            target="_blank" class="more_btn bg5">Book now</a>
+                        <a href="https://www.simplebooking.it/ibe/hotelbooking/search?hid=9914&lang=EN#/offer&offerId=202268"
+                            target="_blank" class="more_btn bg5">Book now!</a>
                     </div>
                 </div>
-                <div class="grid_6">
-                    <img src="{{ asset('images/hotel/Okada-Hotel.jpeg') }}" alt="PSCS - ASCS - Okada Hotel Philippines"
-                        class="img7_pscs_events">
-                </div>
+
             </div>
 
         </div>
@@ -68,13 +88,25 @@
                     business and leisure travelers.
                 </p>
 
+                <p>
+                    The hotel features a variety of room types, including deluxe rooms and suites, all equipped with
+                    modern amenities such as flat-screen TVs, minibars, and complimentary Wi-Fi. Guests can enjoy
+                    dining at the hotel's restaurants, which offer a range of international cuisines.
+                </p>
+
+                <p>
+                    Admiral Hotel Manila also serves <span style="font-weight: bold;">vegetarian and vegan
+                        options</span>, catering to guests with dietary
+                    preferences.
+                </p>
+
                 <ul class="listWithMarker">
-                    <li><a href="https://www.admiralhotelmanila.com/" target="_blank">Offical Website</a></li>
-                    <li><a href="javascript:void(0)">Buffet Options</a></li>
+                    <li><a href="{{ asset('images/hotel/Admiral-Hotel-Rates.pdf') }}" target="_blank">Check Rates</a></li>
                 </ul>
 
                 <div class="form_btns">
-                    <a href="https://www.guestreservations.com/admiral-hotel-manila-mgallery/booking?msclkid=074b795436161ba19c98115747b6a56e" target="_blank" class="more_btn bg5">Book now</a>
+                    <a href="https://forms.gle/wNrdX45rzgUgNFcs9"
+                        target="_blank" class="more_btn bg5">Book now</a>
                 </div>
             </div>
 
@@ -89,6 +121,11 @@
 <div class="container">
     <div class="row">
         <div class="grid_6">
+            <img src="{{ asset('images/hotel/Hotel-101-A.jpeg') }}" alt="PSCS - ASCS - Hotel 101 Philippines"
+                class="img7_pscs_events">
+        </div>
+
+        <div class="grid_6">
             <p class="txt12_welcome_page">
                 Hotel 101
             </p>
@@ -99,21 +136,23 @@
                 both business and leisure travelers.
             </p>
 
+            <p>
+                Hotel 101 is friendly for <span style="font-weight: bold;">Muslim travelers but is not Halal
+                    certified</span>. The hotel provides a
+                variety of dining options, including vegetarian and vegan choices, to cater to guests with different
+                dietary preferences. While the hotel may not have a specific Halal certification, it strives to
+                accommodate the needs of all guests, including those who follow Halal dietary guidelines.
+            </p>
+
             <ul class="listWithMarker">
-                <li><a href="https://hotel101.com/" target="_blank">Offical Website</a></li>
-                <li><a href="javascript:void(0)">Buffet Options</a></li>
+                <li><a href="{{ asset('images/hotel/Hotel-101-Rates.pdf') }}" target="_blank">Check Rates</a></li>
             </ul>
 
             <div class="form_btns">
-                <a href="https://www.bing.com/aclick?ld=e8mCrUdula1Z562n6uM5GB9DVUCUxy8ViMCFNFxSEmP2hIZD8ztVTpOBwCXKOH_853m5lEdQZA8uEmXqE1a5WKHtG2_Gc8l4D7DigUu0cDvB52TxVJxORQV4HWXSLa8XqpllE47hhK4X5yrFA6Ln3ZR0-gpOtx-OzyUuQ8EFz0fjad68JhGduuwaduvEJM-xUT_h3-bQ&u=aHR0cHMlM2ElMmYlMmZIb3RlbDEwMU1hbmlsYS5ndWVzdHJlc2VydmF0aW9ucy5jb20lMmYyMjAxMDYlMmZib29raW5nJTNmbXNjbGtpZCUzZDgzYWZkMWIxNDRjNTFjYzgwNTBmYjliMjM2YzhlNzY0&rlid=83afd1b144c51cc8050fb9b236c8e764" target="_blank" class="more_btn bg5">Book now</a>
+                <a href="https://forms.gle/CLPB9QCHXRX1BtNh7"
+                    target="_blank" class="more_btn bg5">Book now</a>
             </div>
         </div>
-
-        <div class="grid_6">
-            <img src="{{ asset('images/hotel/Hotel-101-A.jpeg') }}" alt="PSCS - ASCS - Hotel 101 Philippines"
-                class="img7_pscs_events">
-        </div>
-
     </div>
 </div>
 
@@ -122,33 +161,42 @@
         <div class="row">
 
             <div class="grid_6">
-                <img src="{{ asset('images/hotel/grand-westside-hotel.jpg') }}"
-                    alt="PSCS - ASCS - Gran Westside Hotel Philippines" class="img7_pscs_events">
+                <img src="{{ asset('images/hotel/Okada-Hotel.jpeg') }}" alt="PSCS - ASCS - Okada Hotel Philippines"
+                    class="img7_pscs_events">
             </div>
-
             <div class="grid_6">
                 <p class="txt12_welcome_page">
-                    Grand Westside
+                    Okada Hotel
                 </p>
                 <p>
-                    Grand Westside Hotel is a luxurious hotel located in the Philippines. It is known for its elegant
-                    design, modern amenities, and exceptional service. The hotel offers a range of accommodations,
-                    dining options, and recreational facilities, making it a popular choice for both business and
-                    leisure
-                    travelers.
+                    Okada Manila is occupying 30 hectares of Asia World City, the largest island development in the
+                    country, and it is the biggest integrated resort in the booming Entertainment City. From its grand
+                    size to its awe-inspiring architecture, Okada Manila is a vibrant addition to the Philippines’
+                    rapidly growing entertainment and leisure industry.
+                </p>
+
+                <p>
+                    Located 3 kilometers away from the Ninoy Aquino International Airport, Okada Manila is an exciting
+                    multi-dimensional property that boasts of a premium selection of hospitality, dining, retail, gaming
+                    and leisure options designed to exceed guest expectations. The Resort is located on the beautiful
+                    Manila Bay, which provides guests with a spectacular view of one of the world’s best sunsets.
+                </p>
+
+                <p>
+                    All of the above are complemented by enticing attractions such as the premium shopping promenade,
+                    the largest and most exciting gaming areas, an impressive night-club and indoor beach club, the
+                    finest and most technologically advanced hotel accommodations, a wide range of dining options and
+                    one of the biggest fountains in the world.
                 </p>
 
                 <ul class="listWithMarker">
-                    <li><a href="https://www.grandwestsidemanila.com/en" target="_blank">Offical Website</a></li>
-                    <li><a href="javascript:void(0)">Buffet Options</a></li>
+                    <li><a href="{{ asset('images/hotel/Okada-Hotel-Rates.pdf') }}" target="_blank">Check the rates</a></li>
                 </ul>
 
                 <div class="form_btns">
-                    <a href="https://bookings.astonhotelsinternational.com/ibe/hotelbooking/search?hid=9914&lang=en" target="_blank" class="more_btn bg5">Book now</a>
+                    <a href="https://forms.gle/yneqvSafWFqrK8DBA" target="_blank" class="more_btn bg5">Book now</a>
                 </div>
             </div>
-
-
 
             <div class="wrapper">
                 <div class="indent1"></div>
